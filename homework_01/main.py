900362642fc2a75e290f7b0ac89942d5d6279153
Orlov.Pavel <spamone@mail.ru>
2022-03-23 21:11:20 +0300

    main.py finished

--- homework_01/main.py
+++ homework_01/main.py
@@ -13,9 +13,9 @@ def power_numbers(nums):
     """
     return nums ** 2
 
-#uncomment for cross-check
-#nums = (1, 2, 3, 4, 5)
-#print(list(map(power_numbers, nums)))
+# Text below just for function testing check
+# nums = (1, 2, 3, 4, 5)
+# print(list(map(power_numbers, nums)))
 
 
 
@@ -24,8 +24,29 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(numbers):
+        if numbers > 1:
+            for num in range(2, round(numbers ** 0.5) + 1):
+                if numbers % num == 0:
+                    return()
+            return numbers
 
-def filter_numbers(*nums, nums_type):
+
+def is_odd(numbers):
+    if numbers % 2 == 0:
+        return numbers
+
+
+def is_even(numbers):
+    if numbers % 2 != 0:
+        return numbers
+
+# Text below just for function testing check
+# numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+# print(list(filter(is_odd, numbers)))
+
+
+def filter_numbers(numbers, numbers_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -36,19 +57,14 @@ def filter_numbers(*nums, nums_type):
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
-    if nums_type == "ODD":
-        return nums % 2 != 0
-    if nums_type == "EVEN":
-        return nums % 2 == 0
-    if nums_type == "PRIME":
-        if nums > 1:
-           for num in range(2, round(nums ** 0.5) + 1):
-            if nums % num == 0:
-                return()
-           return nums
+    if numbers_type == EVEN:
+        return list(filter(is_even, numbers))
+    if numbers_type == ODD:
+        return list(filter(is_odd, numbers))
+    if numbers_type == PRIME:
+        return list(filter(is_prime, numbers))
 
-nums = ((1,2,3,4,5,6,7,8,9,10), "ODD")
-
-
-list(filter(filter_numbers, nums))
-
+# Text below just for function testing check
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+# numbers_type = PRIME
+# print(filter_numbers(numbers, numbers_type))
